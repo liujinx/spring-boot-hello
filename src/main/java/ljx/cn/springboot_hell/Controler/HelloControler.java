5637cf1fe0d613e4461b66e45e1e80fae66affb4
早起的肥虫 <yydljx@126.com>
2018-12-07 08:19:05 +0800

    开放分支

--- src/main/java/ljx/cn/springboot_hell/Controler/HelloControler.java
+++ src/main/java/ljx/cn/springboot_hell/Controler/HelloControler.java
@@ -7,8 +7,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloControler {
 
 	
+	public static void main(String[] args) {
+		System.out.println("");
+	}
+	
 	@GetMapping("/hello")
 	public String hello(){
 		return "Hello SpringBoot";
 	}
+	
+	@GetMapping("/hello4")
+	public String hello4(){
+		return "Hello SpringBoot";
+	}
+	
+	
 }
